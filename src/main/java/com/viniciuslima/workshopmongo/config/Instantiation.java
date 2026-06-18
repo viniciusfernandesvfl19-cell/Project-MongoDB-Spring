2d83084cc7a6bfab75b3c121877d6e6919facdca
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-06-18 18:27:30 -0300

    Referenciando os posts do usuário

--- src/main/java/com/viniciuslima/workshopmongo/config/Instantiation.java
+++ src/main/java/com/viniciuslima/workshopmongo/config/Instantiation.java
@@ -48,5 +48,8 @@ public class Instantiation implements CommandLineRunner {
                 "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
