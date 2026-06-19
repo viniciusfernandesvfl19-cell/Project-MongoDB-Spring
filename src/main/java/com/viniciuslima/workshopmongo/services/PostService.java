71c9e8993f5ea81de079ec1a8af2c7cf5313938a
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-06-19 11:34:10 -0300

    Consulta com anotação @Query

--- src/main/java/com/viniciuslima/workshopmongo/services/PostService.java
+++ src/main/java/com/viniciuslima/workshopmongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return Repo.findByTitleContainingIgnoreCase(text);
+        return Repo.searchTitle(text);
     }
 }
